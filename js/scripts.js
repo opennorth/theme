2bfaf9a1231be636163765afb090671b58af58f2
Marie-Ange Audigé <ma.audige@okam.ca>
2016-05-30 16:54:37 -0400

    MOD - Fix

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ $(function () {
   var slider = $('.b-slider');
   if (slider.unslider) {
     slider.unslider({
-      delay: 100,
+      delay: 6000,
       dots: true,
       keys: false,
       fluid:true,
